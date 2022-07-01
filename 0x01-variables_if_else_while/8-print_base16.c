ede1af11f40b74e838ff1d974f67cb881caa7b27
lilytekle-GitHub <teklelily28@gmail.com>
2022-07-01 15:29:53 -0700

    Prints 0-6,a-f

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -11,10 +11,14 @@ int main(void)
 	int d;
 	char low;
 
-	for (d = '0'; d <= '9'; d++)
+	for (d = 48; d <= 57; d++)
+	{
 		putchar(d);
+	}
 	for (low = 'a'; low <= 'f'; low++)
+	{
 		putchar(low);
-		putchar('\n');
+	}
+	putchar(10);
 	return (0);
 }
