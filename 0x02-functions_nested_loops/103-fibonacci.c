f8d8b7058b9cc81c7b6a35383792b1677df73fbd
lilytekle-GitHub <teklelily28@gmail.com>
2022-07-05 19:08:03 -0700

    Fab 3

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -8,25 +8,18 @@
  */
 int main(void)
 {
-	int i;
-	unsigned long int j, k, next, sum;
+	int i = 0;
+	long j = 1, k = 2, sum = k;
 
-	j = 1;
-	k = 2;
-	next = 3;
-	sum = 0;
-
-	for (i = 1; i <= 33; ++i)
+	while (k + j < 4000000)
 	{
-		if (j < 4000000 && (j % 2) == 0)
-		{
-			sum = sum + j;
-		}
-		sum = sum + j;
-		j = k;
-		k = next;
-	}
-	printf("%lu\n", sum);
+		k += j;
+		if (k % 2 == 0)
+			sum += k;
+		j = k - j;
 
+		++i;
+	}
+	printf("%ld\n", sum);
 	return (0);
 }
