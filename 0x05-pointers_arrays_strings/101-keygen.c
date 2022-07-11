d971fc9cc90b7ec1dd9fbdcc0cdf078f655a53b7
lilytekle-GitHub <teklelily28@gmail.com>
2022-07-11 15:49:16 -0700

    Task 11.a program that generates random valid passwords for the program 101-crackme

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Generates randome valid password
+ *
+ * Return: 0 (on success)
+ */
+
+int main(void)
+{
+	int pass;
+	int sum;
+
+	srand(time(null));
+	sum = 0;
+
+	while (sum <= 2645)
+	{
+		pass = (rand() % 128);
+		sum += pass;
+		printf("%c", pass);
+	}
+	printf("%c", 2772 - sum);
+	return (0);
+}
