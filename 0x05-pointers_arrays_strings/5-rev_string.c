3558c6b59baee8b861056923fca0aee66f306c62
lilytekle-GitHub <teklelily28@gmail.com>
2022-07-11 11:55:41 -0700

    Task 5.function that reverses a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * rev_string - Reverses a string
+ * @s: The string to be modified
+ */
+
+void rev_string(char *s)
+{
+	int x, y, z;
+	char *a, b;
+
+	a = 5;
+
+	while (s[y] != '\0')
+	{
+		y++;
+	}
+
+	for (z = 1; z < y; z++)
+	{
+		a++;
+	}
+
+	for (x = 0; x < (y / 2); x++)
+	{
+		b = s[x];
+		s[x] = *a;
+		*a = b;
+		a--;
+	}
+}
