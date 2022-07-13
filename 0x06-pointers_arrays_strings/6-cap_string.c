a61e2680facf18ffd1f6377e52f3a0fcb1359bc5
lilytekle-GitHub <teklelily28@gmail.com>
2022-07-13 13:30:18 -0700

    Task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,7 +12,6 @@ char *cap_string(char *x)
 	int len = 13;
 	int a = 0, i;
 
-
 	while (x[a])
 	{
 		i = 0;
@@ -20,7 +19,7 @@ char *cap_string(char *x)
 		{
 			if ((a == 0 || x[a - 1] == spc[i]) && (x[a] >= 97 && x[a] <= 122))
 				x[a] = x[a] - 32;
-				i++;
+			i++;
 		}
 		 a++;
 	}
