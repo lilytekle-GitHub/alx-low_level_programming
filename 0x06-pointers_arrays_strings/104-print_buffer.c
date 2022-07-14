3e2a1c51307f8ba3cfd5b02e930c0a02033bc205
lilytekle-GitHub <teklelily28@gmail.com>
2022-07-14 04:58:39 -0700

    Task 12

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,88 +1,44 @@
 #include "main.h"
 #include <stdio.h>
-#include <stdlib.h>
-void print_hex_line(char *buffer, int numBitsInLine, int currentPosition);
-void print_buffer_line(char *b, int n, int cur);
-/**
- * print_buffer - prints contents of a buffer
- *
- * @b: buffer to print
- * @size: size of buffer
- *
- * Return: always void
- */
 
+/**
+ * print_buffer - prints string buffer
+ * @b: the buffer
+ * @size: size of buffer to print
+ *
+ * Return: void
+ */
 void print_buffer(char *b, int size)
 {
-	int quo, rem, i = 0;
-	int bitCounter = 0, numBitsInLine;
+	int o = 0, i, j;
 
-	if (size == 0)
+	if (size <= 0)
+	{
+		printf("\n");
 		return;
-	quo = size / 10;
-	rem = size % 10;
-
-	if (rem)
-		quo++;
-	while (i < quo) /* loop through 10 bits at a time */
-	{
-		numBitsInLine = (size - rem) > bitCounter ? 10 : rem;
-		printf("%.8x: ", bitCounter);
-		print_hex_line(b, numBitsInLine, bitCounter);
-		print_buffer_line(b, numBitsInLine, bitCounter);
-		putchar('\n');
-		bitCounter += 10;
-		i++;
 	}
-}
-/**
- * print_hex_line - prints chars of buffer as hex in sets of 2
- *
- * @b: buffer to print line from
- * @numBitsInLine: number of bits in line, print spaces to fill in
- * @currentPos: position in array of starting point of line
- *
- * Return: always void
- */
-void print_hex_line(char *b, int numBitsInLine, int currentPos)
-{
-	int nestedCounter = 0;
-
-	while (nestedCounter < 10)
+	while (o < size)
 	{
-		if (nestedCounter >= numBitsInLine)
-			printf("  ");
-		else
-			printf("%.2x", b[currentPos + nestedCounter]);
-		if (nestedCounter % 2)
-			putchar(' ');
-		nestedCounter++;
-	}
-}
-/**
- * print_buffer_line - prints chars of buffer as buffchar in sets of 2
- *
- * @b: buffer to print line from
- * @numBitsInLine: number of bits in line, print spaces to fill in
- * @currentPos: position in array of starting point of line
- *
- * Return: always void
- */
-
-void print_buffer_line(char *b, int numBitsInLine, int currentPos)
-{
-	int nestedCounter = 0;
-
-	while (nestedCounter < 10)
-	{
-		if (nestedCounter >= numBitsInLine)
-			break;
-		else if (b[currentPos + nestedCounter] >= 32 && b[currentPos + nestedCounter] < 127)
+		j = size - o < 10 ? size - o : 10;
+		printf("%08x: ", o);
+		for (i = 0; i < 10; i++)
 		{
-			printf("%c", b[currentPos + nestedCounter]);
+			if (i < j)
+				printf("%02x", *(b + o + i));
+			else
+				printf("  ");
+			if (i % 2)
+				printf(" ");
 		}
-		else
-			printf(".");
-		nestedCounter++;
+		for (i = 0; i < j; i++)
+		{
+			int c = *(b + o + i);
+
+			if (c < 32 || c > 132)
+				c = '.';
+			printf("%c", c);
+		}
+		printf("\n");
+		o += 10;
 	}
 }
