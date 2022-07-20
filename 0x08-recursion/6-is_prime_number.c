cdd4831619e6cb785a86409249e1383128d081a1
lilytekle-GitHub <teklelily28@gmail.com>
2022-07-20 08:40:06 -0700

    a function that returns 1 if the input integer is a prime number, otherwise return 0

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -16,18 +16,6 @@ int is_prime_number(int n)
 }
 
 /**
- * is_prime_number - Returns if a number is prime
- * @n: the number to be checked
- *
- * Return: integer value
- */
-
-int is_prime_number(int n)
-{
-	return (check_prime(n, 1));
-}
-
-/**
  * check_prime - Check if number is prime
  * @n: the number to be checked
  * @i: the iteration times
@@ -39,6 +27,7 @@ int check_prime(int n, int i)
 {
 	if (n <= 1)
 		return (0);
+
 	if (n % i == 0 && i > 1)
 		return (0);
 
