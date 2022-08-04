0d1b1f25b60f95b2e408c2ea26ccfafd94f4018f
lilytekle-GitHub <teklelily28@gmail.com>
2022-08-03 17:34:50 -0700

    hot fix

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,39 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <udis86.h>
 
 /**
-  * main - ...
-  * @argc: ...
-  * @argv: ...
-  * Return: ...
-  */
+ * print_opcodes - program that prints the opcodes of its own main function.
+ * @a: address of the main function
+ * @n: number of bytes to print
+ * Return: nothing.
+ */
+
+void print_opcodes(char *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%.2hhx", a[i]);
+		if (i < n - 1)
+		{
+			printf(" ");
+		}
+	}
+	printf("\n");
+}
+
+/**
+ * main - prints the opcodes of its own main function
+ * @argc: integer
+ * @argv: character
+ * Return: always 0.
+ */
 
 int main(int argc, char *argv[])
 {
-	ud_t ud_obj;
-	int val = 0, i = 0;
+	int ae;
 
-	if (argc == 2)
+	if (argc != 2)
 	{
-		val = atoi(argv[1]);
-
-		if (val < 0)
-		{
-			printf("Error\n");
-			exit(2);
-		}
-
-		ud_unit(&ud_obj);
-		ud_set_input_buffer(&ud_obj, argv[1], val);
-		ud_set_mode(&ud_obj, 64);
-		ud_set_syntax(&ud_obj, UD_SYN_INTEL);
-
-		while (ud_disassemble(&ud_obj))
-		{
-			printf("\t%s\n", ud_insn_hex(&ud_obj));
-		}
+		printf("Error\n");
+		exit(1);
 	}
+	ae = atoi(argv[1]);
+	if (ae < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	print_opcodes((char *)&main, ae);
 
 	return (0);
 }
